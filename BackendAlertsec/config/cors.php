90a854b0b12cc9251e85ddc8ad9c705c69e6717d
Pape Seck <souleymaneseck996@gamil.com>
2025-10-06 13:19:39 +0000

    Backend config: broadcasting pusher default; CORS allow dashboard/expo; Dashboard: remove dev proxy

--- BackendAlertsec/config/cors.php
+++ BackendAlertsec/config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'http://localhost:19006',
+        'http://127.0.0.1:19006'
+    ],
 
     'allowed_origins_patterns' => [],
 
